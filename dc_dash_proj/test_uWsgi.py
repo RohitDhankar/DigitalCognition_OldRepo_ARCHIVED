77b62df1ae63b9fb7dd0fe07b96b455a31225fbc
Rohit Dhankar <Dhankar.Rohit@gmail.com>
2019-06-04 22:15:20 +0530

    initial

---
+++ dc_dash_proj/test_uWsgi.py
@@ -0,0 +1,5 @@
+# test.py
+def application(env, start_response):
+    start_response('200 OK', [('Content-Type','text/html')])
+    return [b"Hello World_______"] # python3
+    #return ["Hello World"] # python2
